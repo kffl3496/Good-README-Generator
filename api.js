9fed25f0fe491e6ee821623796f740a55f1575e0
Alex <alexstiles126@gmail.com>
2020-02-24 22:56:40 -0500

    Created a readme.md generator for the user, still a work in progress

---
+++ api.js
@@ -0,0 +1,12 @@
+// var githubAPI = require("https://api.github.com");
+
+const URL = "https://api.github.com/users/" + username;
+
+const api = {
+  getUser(username)
+
+};
+
+module.exports = api;
+
+// "current_user_url": "https://api.github.com/user",
---
+++ index.js
@@ -0,0 +1,23 @@
+
+  const questions = [
+    "What is your gitHub username?",
+    "What is the name of your project?",
+    "What is your project about?"
+];
+
+function writeToFile(fileName, data) {
+    var projectTitle = "";
+    var description = "";
+    var TableofContents = "";
+    var Installation = "";
+    var Usage = "";
+    var License = "";
+    var Contributing = "";
+    var Tests = "";
+}
+
+function init() {
+    writeToFile += questions;
+}
+
+init();
